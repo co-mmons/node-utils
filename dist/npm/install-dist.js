39aa9ab896bb8a1d7e614ab2e5f0ff946176e6a7
Levy <marek@lewczuk.com>
2016-10-18 00:20:49 +0200

    Commit

--- dist/npm/install-dist.js
+++ dist/npm/install-dist.js
@@ -3,6 +3,7 @@
 var path = require("path");
 var process = require("process");
 var file_system_1 = require("../file-system");
+console.log(process.env.PWD);
 if (path.resolve(process.env.PWD, "../../node_modules") == process.env.PWD) {
     var source = path.join(process.env.PWD, "dist");
     var target = process.env.PWD;
--- src/npm/install-dist.ts
+++ src/npm/install-dist.ts
@@ -4,7 +4,7 @@ import * as path from "path";
 import * as process from "process";
 
 import {dirExists, copyFolderRecursiveSync} from "../file-system";
-
+console.log(process.env.PWD);
 if (path.resolve(process.env.PWD, "../../node_modules") == process.env.PWD) {
     let source = path.join(process.env.PWD, "dist");
     let target = process.env.PWD;
