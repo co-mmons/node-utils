4bec24d73f9829c847d9fb1621b04fe083e7e73f
levy <levy@codeandmore.pl>
2020-08-04 23:18:52 +0200

    Commit

--- dist/file-system/copyDirRecursiveSync.js
+++ dist/file-system/copyDirRecursiveSync.js
@@ -1,17 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.copyDirRecursiveSync = void 0;
-var fs_1 = require("fs");
-var path_1 = require("path");
+var fs = require("fs");
+var path = require("path");
 var copyFileSync_1 = require("./copyFileSync");
 function copyDirRecursiveSync(source, target, options) {
-    if (!fs_1.default.existsSync(target)) {
-        fs_1.default.mkdirSync(target);
+    if (!fs.existsSync(target)) {
+        fs.mkdirSync(target);
     }
     // copy
-    if (fs_1.default.lstatSync(source).isDirectory()) {
-        fs_1.default.readdirSync(source).forEach(function (fileName) {
-            var file = path_1.default.join(source, fileName);
+    if (fs.lstatSync(source).isDirectory()) {
+        fs.readdirSync(source).forEach(function (fileName) {
+            var file = path.join(source, fileName);
             if (options && options.exclude) {
                 for (var _i = 0, _a = options.exclude; _i < _a.length; _i++) {
                     var e = _a[_i];
@@ -20,11 +20,11 @@ function copyDirRecursiveSync(source, target, options) {
                     }
                 }
             }
-            if (fs_1.default.lstatSync(file).isDirectory()) {
-                copyDirRecursiveSync(file, path_1.default.join(target, fileName));
+            if (fs.lstatSync(file).isDirectory()) {
+                copyDirRecursiveSync(file, path.join(target, fileName));
             }
             else {
-                copyFileSync_1.copyFileSync(file, path_1.default.join(target, fileName));
+                copyFileSync_1.copyFileSync(file, path.join(target, fileName));
             }
         });
     }
--- src/file-system/copyDirRecursiveSync.ts
+++ src/file-system/copyDirRecursiveSync.ts
@@ -1,5 +1,5 @@
-import fs from "fs";
-import path from "path";
+import * as fs from "fs";
+import * as path from "path";
 import {copyFileSync} from "./copyFileSync";
 
 export function copyDirRecursiveSync(source: string, target: string, options?: { exclude?: Array<string | RegExp> }) {
